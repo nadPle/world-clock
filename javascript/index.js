2092172e911a66c7c41b674d8dfa5ba605faeb1e
nadPle <143026099+nadPle@users.noreply.github.com>
2023-12-30 22:41:14 +0100

    Added some js to change the background-image of selected cities

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,7 @@
 function showStandardTime() {
   let standardTimeElement = document.querySelector("h1");
-  let standardTime = moment().tz("Europe/Zurich");
+  let standardTimeZone = moment.tz.guess();
+  let standardTime = moment().tz(standardTimeZone);
 
   standardTimeElement.innerHTML = standardTime.format("H:mm:ss");
 }
@@ -43,8 +44,7 @@ function updateTime() {
 }
 
 function updateSelectedCity(event) {
-  clearInterval(updateTimeInterval);
-
+  let cityElement = document.querySelector("#variable-city-element");
   let cityTimeZone = event.target.value;
 
   if (cityTimeZone === "current") {
@@ -53,21 +53,57 @@ function updateSelectedCity(event) {
 
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityDate = moment().tz(cityTimeZone);
-  let cityElement = document.querySelector("#variable-city-element");
 
-  cityElement.innerHTML = `
-        <div class="cities-grid-bern" id="bern">
-        <div class="grid-item-city">${cityName}</div>
-        <div class="grid-item-date">${cityDate.format("MMMM Do YYYY")}</div>
-        <div class="grid-item-time">${cityDate.format("H:mm")}</div>
-        </div>
-<a href = "index.html">Show all cities</a>
-        `;
+  function changeSelectedCityTime() {
+    cityElement.innerHTML = `
+          <div class="cities-grid-bern" id="bern">
+          <div class="grid-item-city">${cityName}</div>
+          <div class="grid-item-date">${cityDate.format("MMMM Do YYYY")}</div>
+          <div class="grid-item-time">${cityDate.format("H:mm")}</div>
+          </div>
+  <a href = "index.html" class="reset-link";>Back to all cities</a>
+          `;
+
+    let backgroundElement = document.querySelector("#bern");
+
+    if (event.target.value === "America/Bogota") {
+      backgroundElement.style.backgroundImage =
+        "url(https://edelweiss.scene7.com/is/image/flyedelweiss/colombia-bog-skyline-2:page-header-teaser-1800w)";
+    }
+
+    if (event.target.value === "Australia/Canberra") {
+      backgroundElement.style.backgroundImage =
+        "url(https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_2732,w_4856,x_0,y_0/dpr_1.5/c_limit,w_3000/fl_lossy,q_auto/v1688419581/230703-oconnor-canberra-australia-hero_zbwg53)";
+    }
+
+    if (event.target.value === "Europe/Stockholm") {
+      backgroundElement.style.backgroundImage =
+        "url(https://res.cloudinary.com/kr-reisen/image/upload/f_auto,q_auto:good,ar_2.8,c_crop/w_1920/skandinavien/schweden/hotels/stockholm/skandinavien_schweden_hotels_stockholm-Header-Bild.webp)";
+    }
+
+    if (event.target.value === "Asia/Tokyo") {
+      backgroundElement.style.backgroundImage =
+        "url(https://image.geo.de/30148152/t/is/v3/w1440/r0/-/tokio-f-292656749-jpg--84404-.jpg)";
+    }
+
+    if (event.target.value === "America/Toronto") {
+      backgroundElement.style.backgroundImage =
+        "url(https://www.civitatis.com/blog/wp-content/uploads/2020/12/vista-aerea-toronto.jpg)";
+    }
+
+    if (event.target.value === "current") {
+      backgroundElement.style.backgroundImage =
+        "url(https://spaceaustralia.com/sites/default/files/2019-09/Australia%20from%20space.jpg)";
+    }
+  }
+  clearInterval(updateTimeInterval);
+  changeSelectedCityTime();
 }
 
 let selectedCity = document.querySelector("#selected-city");
 selectedCity.addEventListener("change", updateSelectedCity);
 
+showStandardTime();
 setInterval(showStandardTime, 1000);
 updateTime();
 let updateTimeInterval = setInterval(updateTime, 1000);
